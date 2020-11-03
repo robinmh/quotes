07602b2367636f8978fb07805013f50109dd7af5
robinmh <R.Harrington@ieee.org>
2020-11-04 10:01:18 +1300

    Initial setup

---
+++ quotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  quotes
+//
+//  Created by Robin Harrington on 4/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ quotes/quotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  quotesApp.swift
+//  quotes
+//
+//  Created by Robin Harrington on 4/11/20.
+//
+
+import SwiftUI
+
+@main
+struct quotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
